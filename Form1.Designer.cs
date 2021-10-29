4bc4f09c69df3f2d87ac892e7aea84150e2e5b5e
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-10-29 11:22:21 +0200

    Konekcija

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,111 @@ namespace _4_9_Forma
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_ime = new System.Windows.Forms.TextBox();
+            this.txt_prezime = new System.Windows.Forms.TextBox();
+            this.txt_ocena = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(193, 69);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(139, 20);
+            this.txt_id.TabIndex = 0;
+            // 
+            // txt_ime
+            // 
+            this.txt_ime.Location = new System.Drawing.Point(193, 110);
+            this.txt_ime.Name = "txt_ime";
+            this.txt_ime.Size = new System.Drawing.Size(139, 20);
+            this.txt_ime.TabIndex = 1;
+            // 
+            // txt_prezime
+            // 
+            this.txt_prezime.Location = new System.Drawing.Point(193, 153);
+            this.txt_prezime.Name = "txt_prezime";
+            this.txt_prezime.Size = new System.Drawing.Size(139, 20);
+            this.txt_prezime.TabIndex = 2;
+            // 
+            // txt_ocena
+            // 
+            this.txt_ocena.Location = new System.Drawing.Point(193, 195);
+            this.txt_ocena.Name = "txt_ocena";
+            this.txt_ocena.Size = new System.Drawing.Size(139, 20);
+            this.txt_ocena.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Prezime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ocena";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ocena);
+            this.Controls.Add(this.txt_prezime);
+            this.Controls.Add(this.txt_ime);
+            this.Controls.Add(this.txt_id);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_ime;
+        private System.Windows.Forms.TextBox txt_prezime;
+        private System.Windows.Forms.TextBox txt_ocena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace _4_9_Forma
 {
@@ -15,6 +16,15 @@ namespace _4_9_Forma
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable ucenik = new DataTable();
+            string CS = "Data source=INF_4_PROFESOR\\SQLPBG; Initial catalog=MilosP2021; Integrated security=true";
+            SqlConnection veza = new SqlConnection(CS);
+
+
         }
     }
 }
